3b25f572b52e6926eebfc51a0ca5549cb5e2695a
Jesse Phillips <Jesse.K.Phillips@gmail.com>
2019-10-27 14:21:38 -0700

    :bulb: Add Documentation

--- source/structopt/attributes.d
+++ source/structopt/attributes.d
@@ -1,21 +1,55 @@
+/**
+ * Compile Time attributes for specifying proprites to std.getopt.
+ *
+ * These structs are utilized by the the generator to populate
+ * getopt's properties.
+ */
 module structopt.attributes;
 
 import std.traits;
 
+/**
+ * Command Line Help Message for the arugement.
+ */
 struct Help {
+    /**
+     * Help Message.
+     */
     string msg;
 }
 
+/**
+ * Command Line Argument Option which causes assignement to this field.
+ */
 struct Option {
+    /**
+     * All argument options.
+     */
     string[] names;
 
+    /**
+     * Construct all options into an array.
+     *
+     * ```
+     * @Option("help", "h")
+     */
     this(string[] names...) {
         this.names = names;
     }
 
+    /**
+     * Used by generator to translate array into getopt argument.
+     */
     string cononical() {
         import std.algorithm;
         import std.conv;
         return names.joiner("|").to!string;
+    }
+    ///
+    unittest {
+        auto opt = Option("help", "h");
+
+        assert(opt.names == ["help", "h"]);
+        assert(opt.cononical() == "help|h");
     }
 }
--- source/structopt/package.d
+++ source/structopt/package.d
@@ -1,12 +1,53 @@
+/**
+ * This generative method creates a getopt call based on the
+ * attributes assign to  a structure.
+ *
+ * ```
+import structopt;
+
+// Specify The Parameter Structure
+struct Options
+{
+    @Option("threads", "t")
+    @Help("Number of threads to use.")
+    size_t threads;
+
+    @Option("file")
+    @Help("Input files")
+    string[] files;
+}
+
+void main(string[] args) {
+    Options props;
+
+    // Pass in the struct to generate UDA for
+    auto helpInfo = mixin(GenerateGetopt!(props, args));
+
+        defaultGetoptPrinter("Options: ",
+          helpInfo.options);
+        // Output to console:
+
+        //Options:
+        //-t --threads Number of threads to use.
+        //      --file Input files
+        //-h    --help This help information./
+}
+ * ```
+ */
 module structopt;
 
 public import structopt.attributes;
 public import std.getopt;
 public import std.traits;
 
+/// Rename the Option struct of getopt
 alias GetOption = std.getopt.Option;
+/// Reastablish the name for the struct attributes
 alias Option = structopt.attributes.Option;
 
+/**
+ * Generate the std.getopt method call.
+ */
 string GenerateGetopt(alias Options, alias args)() pure nothrow {
     import std.meta;
     import std.typecons;
@@ -22,4 +63,33 @@ string GenerateGetopt(alias Options, alias args)() pure nothrow {
     }
 
     return ans ~ ")";
+}
+///
+unittest {
+    // Specify The Parameter Structure
+    struct Options
+    {
+        @Option("threads", "t")
+        @Help("Number of threads to use.")
+        size_t threads;
+
+        @Option("file")
+        @Help("Input files")
+        string[] files;
+    }
+
+    Options props;
+    string[] arg = ["./structopt", "t", "24"];
+
+    // Pass in the struct to generate UDA for
+    auto helpInfo = mixin(GenerateGetopt!(props, arg));
+    auto expected = [
+        GetOption("-t", "--threads", "Number of threads to use.", false),
+        GetOption("", "--file", "Input files", false),
+        GetOption("-h", "--help", "This help information.", false)
+    ];
+
+    import std.algorithm;
+    assert(helpInfo.options.equal(expected));
+
 }
